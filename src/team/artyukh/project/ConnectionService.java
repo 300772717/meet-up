31ed9aa4cbcf0c653d8400ba0bbceec55086a9b9
300772717 <bakoulinilya@gmail.com>
2015-02-18 19:52:02 -0500

    Comment change again

--- src/team/artyukh/project/ConnectionService.java
+++ src/team/artyukh/project/ConnectionService.java
@@ -33,7 +33,7 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 public class ConnectionService extends Service {
-	//COMMENT CHANGE
+	//COMMENT CHANGE AGAIN
 	public static final String INTENT_MESSAGE = "INTENT_MESSAGE";
 	private static final String PREFS_FILE = "team.artyukh.project.PREFS_FILE";
 	private SharedPreferences data;
